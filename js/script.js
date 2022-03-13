6011ddb5d6e2045c6c363c494d0054221056012d
annomao <omaoannkerubo@gmail.com>
2022-03-13 19:07:49 +0300

    remove hover effects

--- js/script.js
+++ js/script.js
@@ -1,27 +1,18 @@
 $(document).ready( function(){
   //functions for hide and show div on hover
-  $(".descriptions-icon").mouseover(
-    function() {
+
+  $( ".descriptions-icon" ).on({
+    click: function() {
       var index = $(".descriptions-icon").index(this);
       $(".descriptions-icon").eq(index).hide();
-      $(".descriptions").eq(index).show();
-  }).mouseout(
-    function() {
-      var index = $(".descriptions-icon").index(this);
+      $(".descriptions").eq(index).show();}
+  });
+  $( ".descriptions" ).on({
+    click: function() {
+      var index = $(".descriptions").index(this);
       $(".descriptions-icon").eq(index).show();
-      $(".descriptions").eq(index).hide();
-    });
-  //functions to show hide dive when clicked
-  // $(".descriptions-icon img").click(function() {
-  //   var index = $(".descriptions-icon").index(this);
-  //   $(".descriptions").eq(index).show();
-  //   $(".descriptions-icon").eq(index).hide();
-  // });
-  // $(".descriptions").click(function() {
-  //   var index = $(".descriptions").index(this);
-  //   $(".descriptions-icon").eq(index).show();
-  //   $(".descriptions").eq(index).hide();
-  // });
+      $(".descriptions").eq(index).hide();}
+  });
 
   //function for received message popup
   $("#contact-form").submit(function(e){

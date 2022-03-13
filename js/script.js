2388eee70b28b8f9663eec763f046c1ba8e10230
annomao <omaoannkerubo@gmail.com>
2022-03-13 18:26:00 +0300

    change function from hover to over and out

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 $(document).ready( function(){
-  //functions for hide and show div
-  $(".descriptions-icon").hover(
+  //functions for hide and show div on hover
+  $(".descriptions-icon").mouseover(
     function() {
-    var index = $(".descriptions-icon").index(this);
-    $(".descriptions-icon").eq(index).hide();
-    $(".descriptions").eq(index).show();
-  },
+      var index = $(".descriptions-icon").index(this);
+      $(".descriptions-icon").eq(index).hide();
+      $(".descriptions").eq(index).show();
+  }).mouseout(
     function() {
       var index = $(".descriptions-icon").index(this);
       $(".descriptions-icon").eq(index).show();
       $(".descriptions").eq(index).hide();
-    }
-  );
+    });
+  //functions to show hide dive when clicked
+  // $(".descriptions-icon img").click(function() {
+  //   var index = $(".descriptions-icon").index(this);
+  //   $(".descriptions").eq(index).show();
+  //   $(".descriptions-icon").eq(index).hide();
+  // });
+  // $(".descriptions").click(function() {
+  //   var index = $(".descriptions").index(this);
+  //   $(".descriptions-icon").eq(index).show();
+  //   $(".descriptions").eq(index).hide();
+  // });
 
   //function for received message popup
   $("#contact-form").submit(function(e){
